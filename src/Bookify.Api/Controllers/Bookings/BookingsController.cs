7ee645e88fca23da35f314ee289bb65cf221c8ce
Grigory Zhukov <grigory@zhukoff.pro>
2025-03-09 21:26:11 +0300

    Добавил пропущенные методы Bookings

--- src/Bookify.Api/Controllers/Bookings/BookingsController.cs
+++ src/Bookify.Api/Controllers/Bookings/BookingsController.cs
@@ -1,6 +1,12 @@
 ﻿using Asp.Versioning;
+using Bookify.Application.Bookings.CancelBooking;
+using Bookify.Application.Bookings.CompleteBooking;
+using Bookify.Application.Bookings.ConfirmBooking;
 using Bookify.Application.Bookings.GetBooking;
+using Bookify.Application.Bookings.GetBookings;
+using Bookify.Application.Bookings.RejectBooking;
 using Bookify.Application.Bookings.ReserveBooking;
+using Bookify.Domain.Abstractions;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -20,16 +26,31 @@ public class BookingsController : ControllerBase
         _sender = sender;
     }
 
-    [HttpGet]
+    [HttpGet("{id}")]
     public async Task<IActionResult> GetBooking(Guid id, CancellationToken cancellationToken)
     {
         var query = new GetBookingQuery(id);
-        var result = await _sender.Send(query, cancellationToken);
-        return result.IsSuccess ? Ok(result) : NotFound();
+
+        Result<Application.Bookings.GetBooking.BookingResponse> result = await _sender.Send(query, cancellationToken);
+
+        return result.IsSuccess ? Ok(result.Value) : NotFound();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetBookings(CancellationToken cancellationToken)
+    {
+        var query = new GetAllBookingsQuery();
+
+        Result<IReadOnlyList<Application.Bookings.GetBookings.BookingResponse>> result =
+            await _sender.Send(query, cancellationToken);
+
+        return result.IsSuccess ? Ok(result.Value) : NotFound();
     }
 
     [HttpPost]
-    public async Task<IActionResult> ReserveBooking(ReserveBookingRequest request, CancellationToken cancellationToken)
+    public async Task<IActionResult> ReserveBooking(
+        ReserveBookingRequest request,
+        CancellationToken cancellationToken)
     {
         var command = new ReserveBookingCommand(
             request.ApartmentId,
@@ -37,12 +58,76 @@ public class BookingsController : ControllerBase
             request.StartDate,
             request.EndDate);
 
-        var result = await _sender.Send(command, cancellationToken);
+        Result<Guid> result = await _sender.Send(command, cancellationToken);
 
         if (result.IsFailure)
         {
-            return BadRequest(result);
+            return BadRequest(result.Error);
         }
+
         return CreatedAtAction(nameof(GetBooking), new { id = result.Value }, result.Value);
+    }
+
+    [HttpPut("{id}/complete")]
+    public async Task<IActionResult> CompleteBooking(Guid id, CancellationToken cancellationToken)
+    {
+        var update = new CompleteBookingCommand(id);
+
+        Result result = await _sender.Send(update, cancellationToken);
+
+        if (result.IsFailure)
+
+        {
+            return BadRequest(result.Error);
+        }
+
+        return Ok();
+    }
+
+    [HttpPut("{id}/cancel")]
+    public async Task<IActionResult> CancelBooking(Guid id, CancellationToken cancellationToken)
+    {
+        var update = new CancelBookingCommand(id);
+
+        Result result = await _sender.Send(update, cancellationToken);
+
+        if (result.IsFailure)
+
+        {
+            return BadRequest(result.Error);
+        }
+
+        return Ok();
+    }
+
+    [HttpPut("{id}/confirm")]
+    public async Task<IActionResult> ConfirmBooking(Guid id, CancellationToken cancellationToken)
+    {
+        var update = new ConfirmBookingCommand(id);
+
+        Result result = await _sender.Send(update, cancellationToken);
+
+        if (result.IsFailure)
+
+        {
+            return BadRequest(result.Error);
+        }
+
+        return Ok();
+    }
+
+    [HttpPut("{id}/reject")]
+    public async Task<IActionResult> RejectBooking(Guid id, CancellationToken cancellationToken)
+    {
+        var update = new RejectBookingCommand(id);
+
+        Result result = await _sender.Send(update, cancellationToken);
+
+        if (result.IsFailure)
+        {
+            return BadRequest(result.Error);
+        }
+
+        return Ok();
     }
 }
